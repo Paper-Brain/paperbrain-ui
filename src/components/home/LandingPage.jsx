7a2286b1910268d193468e2a5a18ab8707831f8b
mddanish867 <mddanish867@gmail.com>
2025-12-24 12:02:21 +0530

    decreased opacity for background matrices

--- src/components/home/LandingPage.jsx
+++ src/components/home/LandingPage.jsx
@@ -44,7 +44,7 @@ const NovaPro = () => {
       {/* --- BACKGROUND EFFECTS --- */}
       {/* 1. Matrix Grid Pattern */}
       <div className="fixed inset-0 z-0 pointer-events-none">
-        <div className="absolute inset-0 bg-[linear-gradient(to_right,#ffffff10_1px,transparent_1px),linear-gradient(to_bottom,#ffffff10_1px,transparent_1px)] bg-[size:40px_40px]"></div>
+        <div className="absolute inset-0 bg-[linear-gradient(to_right,#ffffff07_1px,transparent_1px),linear-gradient(to_bottom,#ffffff07_1px,transparent_1px)] bg-[size:40px_40px]"></div>
 
       </div>
 
